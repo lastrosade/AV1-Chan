075703f12820f524bbb381d157605a42008075b2
Jeremy Lee Shields <jeremy.shields@orange.fr>
2020-01-15 14:03:36 +0100

    2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import the discord.js module
 const Discord = require('discord.js')
 const fs = require('fs')
+const https = require('https');
+const path = require('path');
+const { exec } = require("child_process");
 
 // Create an instance of Discord that we will use to control the bot
 const bot = new Discord.Client();
@@ -8,40 +11,121 @@ const bot = new Discord.Client();
 // Token for your bot, located in the Discord application console - https://discordapp.com/developers/applications/me/
 const token = fs.readFileSync('bot-token.txt',"ascii")
 
+function humanFileSize(size) {
+    return size
+}
+
 // Gets called when our bot is successfully logged in and connected
 bot.on('ready', () => {
     console.log('Hello World!');
 });
 
+let bazinga = 0;
+
 // Event to listen to messages sent to the server where the bot is located
 bot.on('message', message => {
     // So the bot doesn't reply to iteself
     if (message.author.bot) return;
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+    if (message.content === 'ping' || message.content === 'Ping') {
+        message.reply('Pong!');
         return;
     }
 
-    // Check if the message starts with the `!` trigger
-    if (message.content.indexOf('!') === 0) {
-        // Get the user's message excluding the `!`
-        var text = message.content.substring(1);
+    // if (message.channel.name === "off-topic") {
+    //     if (message.member.user.id === "134113602494791680" || message.member.user.id === "551696650826153995" || message.member.user.id === "176413727401312257") {
+    //         message.react("👍")
+    //     }
+    // }
 
-        // Reverse the message
-        var reversed = '';
-        var i = text.length;
+    if (message.channel.name === "av1-chan_lib") {
 
-        while (i > 0) {
-            reversed += text.substring(i - 1, i);
-            i--;
-        }
+        message.attachments.forEach((item) => {
 
-        // Reply to the user's message
-        message.reply("What the fuck did you just fucking say about AV1, you little bitch? I'll have you know I graduated top of my class in FFmpeg, and I've been involved in numerous secret encodes on nyaa.si, and I have over 300 confirmed encodes. I am trained in gorilla debugging of day 0 appveyor releases and I'm the top shitposter in the entire AV1 subreddit. You are nothing to me but just another video file. I will compress you the fuck out with efficiency the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my #daala IRC across the TOR and your IP is being traced right now so you better prepare for the DDOS, maggot. The storm that wipes out the pathetic little thing you call your seedbox. You're fucking dead, kid. I can be anywhere, anytime, Chrome, Firefox, Opera, you name it. And I am supported in over seven hundred ways, and that's just with my in pre-alpha decoder. Not only am I extensively trained in batch scripting, but I have access to the entire arsenal of the Amazon Web Service and I will use it to its full extent to wipe your miserable h265 encodes off the face of the internet, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your cheetos covered fingers of the keyboard. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.");
+            message.channel.send("Scanning file\n").then((sentmessage) => {
+
+                const filepath = path.join(__dirname, "temp", item.filename);
+                const file = fs.createWriteStream(filepath);
+                https.get(item.url, function(response) {
+                    response.pipe(file);
+                    exec("ffprobe -print_format json -show_format -show_streams "+filepath, (error, stdout, stderr) => {
+                        if (stderr) {
+                            // message.channel.send(stderr);
+                        }
+                        if (error) {
+                            console.error(error);
+                            message.channel.send("I broke, help.");
+                            return
+                        }
+
+                        obj = JSON.parse(stdout);
+
+                        console.log(obj);
+
+                        let msg = `${item.filename}\n\`\`\`\nContainer    : ${obj.format.format_name}\nSize         : ${humanFileSize(obj.format.size)}\n`;
+                        if (obj.format.bit_rate) {
+                            msg += `Bitrate      : ${humanFileSize(obj.format.bit_rate)}\n`;
+                        }
+                        if (obj.format.duration)
+                        {
+                            msg += `Duration     : ${obj.format.duration}\n`;
+                        }
+                        if (obj.format.tags) {
+                            if (obj.format.tags.ENCODER) {
+                                msg += `Encoder      : ${obj.format.tags.ENCODER}\n`
+                            }
+                        }
+
+                        msg += `\`\`\``;
+
+                        obj.streams.forEach((value) => {
+
+                            msg += `\`\`\`\n+ Stream ${value.index}   : ${value.codec_type}\n`;
+
+                            if (value.codec_name === "h264" || value.codec_name === "h265") {
+                                msg += `| Codec      : Filthy ${value.codec_name}\n`;
+                            } else {
+                                msg += `| Codec      : ${value.codec_name}\n`;
+                            }
+                            if (value.profile) {
+                                msg += `| Profile    : ${value.profile}\n`;
+                            }
+                            if (value.codec_type === "video") {
+                                if (value.pix_fmt) {
+                                    msg += `| pix_fmt    : ${value.pix_fmt}\n`;
+                                }
+                                if (value.color_range) {
+                                    msg += `| color range: ${value.color_range}\n`;
+                                }
+                                if (value.color_space) {
+                                    msg += `| color space: ${value.color_space}\n`;
+                                }
+                                if (value.width && value.height) {
+                                    msg += `| res        : ${value.width}x${value.height}\n`;
+                                }
+                                if (value.sample_aspect_ratio && value.display_aspect_ratio) {
+                                    msg += `| sar        : ${value.sample_aspect_ratio}\n`
+                                    msg += `| dar        : ${value.display_aspect_ratio}\n`;
+                                }
+    
+                            } else if (value.codec_type === "audio") {
+                                if (value.channels) {
+                                    msg += `| channels   : ${value.channels}\n`;
+                                }
+
+                            } else if (value.codec_type === "subtitles") {
+                                // console.log(value);
+                            }
+
+                            msg += "\`\`\`"
+                            
+                        });
+                        sentmessage.edit(msg)
+                        // message.channel.send(msg);
+                    });
+                });
+            });
+        });
     }
 });
-  
 
 bot.login(token);
-
-
