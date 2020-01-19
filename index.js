cf8d4a1b231c1c64df77afffde15c9261a326af6
Jeremy Lee Shields <jeremy.shields@orange.fr>
2020-01-19 02:18:44 +0100

    Whitelist, file deletion, help

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+'use strict'
+
 // Import the discord.js module
 const Discord = require('discord.js')
 const fs = require('fs')
 const https = require('https');
 const path = require('path');
-const brain = require('brain.js')
+const brain = require('brain.js');
 const { exec } = require("child_process");
 
 // Create an instance of Discord that we will use to control the bot
@@ -11,10 +13,10 @@ const bot = new Discord.Client();
 const rich = new Discord.RichEmbed();
 
 // Token for your bot, located in the Discord application console - https://discordapp.com/developers/applications/me/
-const token = fs.readFileSync('bot-token.txt',"ascii")
+const token = fs.readFileSync('bot-token.txt', "ascii");
 
-function humanFileSize(size) {
-    return size
+const humanFileSize = (size) => {
+    return size;
 }
 
 // Gets called when our bot is successfully logged in and connected
@@ -39,7 +41,7 @@ bot.on('message', message => {
 
     // if (message.channel.name === "av1-chan_lib")
     if (message.content.toLowerCase() === '!help') {
-        let msg = "I scan files.\nTo scan a file upload it with the message '!scan' or 'scan', it **has** to be a single message.";
+        let msg = "I scan files.\nTo scan a file: upload it with the message '!scan' or 'scan', it **has** to be a single message.\nI only scan \`webm, ivf, webp, jpg, jpeg, png, webm, bmp, mkv, mka, ogv, oga, ogg, opus, mp4, ts, m2ts, mts, flac, c2, wav, mp3\`\nPlease do not use me for spam (ie don't scan memes and trash), Be respectful, Thanks.";
         message.author.send(msg);
         return;
     }
@@ -48,99 +50,104 @@ bot.on('message', message => {
     //     message.channel.send("Fail", { files: [path.join(__dirname, "temp", "nice.PNG")] });
     // }
 
-    // if (message.channel.name === "av1-chan_lib" || message.channel.name === "video" || message.channel.name === "files")
+    // if (message.channel.name === "av1-chan_lib" , | message.channel.name === "video" || message.channel.name === "files")
     if (message.content.toLowerCase() === '!scan' || message.content.toLowerCase() === 'scan') {
 
         message.attachments.forEach((item) => {
 
             const re = /(?:\.([^.]+))?$/;
             const ext = re.exec(item.filename)[1].toLowerCase();
-            if ( ext.match(/^(png|exe|txt|log|7z|7zip|tar|gz|lz|bz2|xz|zip)$/)) {
-                return
-            }
+            if (ext.match(/^(webm|ivf|webp|jpg|jpeg|png|webm|bmp|mkv|mka|ogv|oga|ogg|opus|mp4|ts|m2ts|mts|vob|flac|c2|wav|mp3)$/)) { 
+                message.channel.send("Scanning file\n").then((sentmessage) => {
 
-            message.channel.send("Scanning file\n").then((sentmessage) => {
-
-                const filepath = path.join(__dirname, "temp", item.filename);
-                const file = fs.createWriteStream(filepath);
-                https.get(item.url, function(response) {
-                    response.pipe(file);
-                    exec("ffprobe -print_format json -show_format -show_streams "+filepath, (error, stdout, stderr) => {
-                        if (stderr) {
-                            // message.channel.send(stderr);
-                        }
-                        if (error) {
-                            console.error(error);
-                            message.channel.send("I broke, help.");
-                            return;
-                        }
-
-                        obj = JSON.parse(stdout);
-
-                        console.log(obj);
-
-                        let msg = `${item.filename}\n\`\`\`\nContainer    : ${obj.format.format_name}\nSize         : ${humanFileSize(obj.format.size)}\n`;
-                        if (obj.format.bit_rate) {
-                            msg += `Bitrate      : ${humanFileSize(obj.format.bit_rate)}\n`;
-                        }
-                        if (obj.format.duration) {
-                            msg += `Duration     : ${obj.format.duration}\n`;
-                        }
-                        if (obj.format.tags) {
-                            if (obj.format.tags.ENCODER) {
-                                msg += `Encoder      : ${obj.format.tags.ENCODER}\n`
+                    const filepath = path.join(__dirname, "temp", item.filename);
+                    const file = fs.createWriteStream(filepath);
+                    https.get(item.url, function (response) {
+                        response.pipe(file);
+                        exec("ffprobe -print_format json -show_format -show_streams " + filepath, (error, stdout, stderr) => {
+                            if (stderr) {
+                                // message.channel.send(stderr);
                             }
-                        }
-
-                        msg += `\`\`\``;
-
-                        obj.streams.forEach((value) => {
-
-                            msg += `\`\`\`\n+ Stream ${value.index}   : ${value.codec_type}\n`;
-
-                            if (value.codec_name === "h264" || value.codec_name === "h265") {
-                                msg += `| Codec      : Filthy ${value.codec_name}\n`;
-                            } else {
-                                msg += `| Codec      : ${value.codec_name}\n`;
-                            }
-                            if (value.profile) {
-                                msg += `| Profile    : ${value.profile}\n`;
-                            }
-                            if (value.codec_type === "video") {
-                                if (value.pix_fmt) {
-                                    msg += `| pix_fmt    : ${value.pix_fmt}\n`;
-                                }
-                                if (value.color_range) {
-                                    msg += `| color range: ${value.color_range}\n`;
-                                }
-                                if (value.color_space) {
-                                    msg += `| color space: ${value.color_space}\n`;
-                                }
-                                if (value.width && value.height) {
-                                    msg += `| res        : ${value.width}x${value.height}\n`;
-                                }
-                                if (value.sample_aspect_ratio && value.display_aspect_ratio) {
-                                    msg += `| sar        : ${value.sample_aspect_ratio}\n`
-                                    msg += `| dar        : ${value.display_aspect_ratio}\n`;
-                                }
-    
-                            } else if (value.codec_type === "audio") {
-                                if (value.channels) {
-                                    msg += `| channels   : ${value.channels}\n`;
-                                }
-
-                            } else if (value.codec_type === "subtitles") {
-                                // console.log(value);
+                            if (error) {
+                                console.error(error);
+                                message.channel.send("I broke, help.");
+                                return;
                             }
 
-                            msg += "\`\`\`"
+                            let obj = JSON.parse(stdout);
 
+                            console.log(obj);
+
+                            let msg = `\`${item.filename}\`\n\`\`\`\nContainer    : ${obj.format.format_name}\nSize         : ${humanFileSize(obj.format.size)}\n`;
+                            if (obj.format.bit_rate) {
+                                msg += `Bitrate      : ${humanFileSize(obj.format.bit_rate)}\n`;
+                            }
+                            if (obj.format.duration) {
+                                msg += `Duration     : ${obj.format.duration}\n`;
+                            }
+                            if (obj.format.tags) {
+                                if (obj.format.tags.ENCODER) {
+                                    msg += `Encoder      : ${obj.format.tags.ENCODER}\n`
+                                }
+                            }
+
+                            msg += `\`\`\``;
+
+                            obj.streams.forEach((value) => {
+
+                                msg += `\`\`\`\n+ Stream ${value.index}   : ${value.codec_type}\n`;
+
+                                if (value.codec_name === "h264" || value.codec_name === "h265") {
+                                    msg += `| Codec      : Filthy ${value.codec_name}\n`;
+                                } else {
+                                    msg += `| Codec      : ${value.codec_name}\n`;
+                                }
+                                if (value.profile) {
+                                    msg += `| Profile    : ${value.profile}\n`;
+                                }
+                                if (value.codec_type === "video") {
+                                    if (value.pix_fmt) {
+                                        msg += `| pix_fmt    : ${value.pix_fmt}\n`;
+                                    }
+                                    if (value.color_range) {
+                                        msg += `| color range: ${value.color_range}\n`;
+                                    }
+                                    if (value.color_space) {
+                                        msg += `| color space: ${value.color_space}\n`;
+                                    }
+                                    if (value.width && value.height) {
+                                        msg += `| res        : ${value.width}x${value.height}\n`;
+                                    }
+                                    if (value.sample_aspect_ratio && value.display_aspect_ratio) {
+                                        msg += `| sar        : ${value.sample_aspect_ratio}\n`
+                                        msg += `| dar        : ${value.display_aspect_ratio}\n`;
+                                    }
+
+                                } else if (value.codec_type === "audio") {
+                                    if (value.channels) {
+                                        msg += `| channels   : ${value.channels}\n`;
+                                    }
+
+                                } else if (value.codec_type === "subtitles") {
+                                    // console.log(value);
+                                }
+
+                                msg += "\`\`\`"
+
+                            });
+                            sentmessage.edit(msg);
+                            // message.channel.send(msg);
+                            fs.unlink(filepath, () => {return});
                         });
-                        sentmessage.edit(msg)
-                        // message.channel.send(msg);
                     });
                 });
-            });
+            } else {
+                // message.delete();
+                let msg = "I scan files.\nTo scan a file: upload it with the message '!scan' or 'scan', it **has** to be a single message.\nI only scan \`webm, ivf, webp, jpg, jpeg, png, webm, bmp, mkv, mka, ogv, oga, ogg, opus, mp4, ts, m2ts, mts, flac, c2, wav, mp3\`\nPlease do not use me for spam (ie don't scan memes and trash), Be respectful, Thanks.";
+                message.author.send(msg);
+                return;
+            }
+
         });
         return;
     }
