7d49ed079f81459755e15988a27babc2884fe407
Jeremy Lee Shields <jeremy.shields@orange.fr>
2020-02-05 15:04:09 +0100

    Time

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const https = require('https');
 const path = require('path');
 const brain = require('brain.js');
 const { exec } = require("child_process");
+const moment = require('moment');
 
 // Create an instance of Discord that we will use to control the bot
 const bot = new Discord.Client();
 const rich = new Discord.RichEmbed();
+
+process.env.TZ = 'Europe/London'
 
 // Token for your bot, located in the Discord application console - https://discordapp.com/developers/applications/me/
 const token = fs.readFileSync('bot-token.txt', "ascii");
@@ -73,6 +76,12 @@ bot.on('message', message => {
     // if (message.content === "nice" && message.member.user.id === '132637059327328256') {
     //     message.channel.send("Fail", { files: [path.join(__dirname, "temp", "nice.PNG")] });
     // }
+
+    if (message.content.toLowerCase() === '!time') {
+        let now = moment();
+        message.channel.send(`UTC Time is ${moment().format("HH:mm")}`);
+        // message.channel.send("Wowwwww, you meow like a cat! That means you are one, right? Shut the fuck up. If you really want to be put on a leash and treated like a domestic animal then that’s called a fetish, not “quirky” or “cute”. What part of you seriously thinks that any part of acting like a feline establishes a reputation of appreciation? Is it your lack of any defining aspect of personality that urges you to resort to shitty representations of cats to create an illusion of meaning in your worthless life? Wearing “cat ears” in the shape of headbands further notes the complete absence of human attribution to your false sense of personality, such as intelligence or charisma in any form or shape. Where do you think this mindset’s gonna lead you? You think you’re funny, random, quirky even? What makes you think that acting like a fucking cat will make a goddamn hyena laugh? I, personally, feel extremely sympathetic towards you as your only escape from the worthless thing you call your existence is to pretend to be an animal. But it’s not a worthy choice to assert this horrifying fact as a dominant trait, mainly because personality traits require an initial personality to lay their foundation on. You’re not worthy of anybody’s time, so go fuck off, “cat-girl”.");
+    }
 
     // if (message.channel.name === "av1-chan_lib" , | message.channel.name === "video" || message.channel.name === "files")
     if (message.content.toLowerCase() === '!scan' || message.content.toLowerCase() === 'scan') {
